1214e573688349dd4c1aa38655172f86a0871218
Егор <egor.zholobov@yandex.ru>
2023-05-18 14:36:29 +0300

    MinorFixes

---
+++ ActiviPrilizhenie/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ActiviPrilizhenie.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ActiviPrilizhenie
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            NavigationHelper.AddPageChangedEventHandler(ChangePageHandler);
+            NavigationHelper.ChangePage(new LoginPage());
+           
+        }
+        public void ChangePageHandler()
+        {
+            MainFrame.Content = NavigationHelper.GetCurrentPage();
+        }
+    }
+}
---
+++ ActiviPrilizhenie/NavigationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ActiviPrilizhenie
+{
+    public class NavigationHelper
+    {
+        private static Page CurrentPage { get; set; }
+        public delegate void NavigationDelegate();
+        private static event NavigationDelegate PageChanged;
+
+        public static void ChangePage(Page page)
+        {
+            CurrentPage = page;
+            PageChanged();
+        }
+        public static void AddPageChangedEventHandler(NavigationDelegate handler)
+        {
+            PageChanged += handler;
+        }
+        public static Page GetCurrentPage()
+        {
+            return CurrentPage;
+        }
+    }
+}
